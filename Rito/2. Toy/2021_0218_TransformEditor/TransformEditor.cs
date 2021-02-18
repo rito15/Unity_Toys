4e5cc4dfafdaad739c0200ee82ee7c1a997f438f
rito15 <ghdtkals15@gmail.com>
2021-02-19 05:10:35 +0900

    [2021-02-19] writing transform editor

--- Rito/2. Toy/2021_0218_TransformEditor/TransformEditor.cs
+++ Rito/2. Toy/2021_0218_TransformEditor/TransformEditor.cs
@@ -17,12 +17,28 @@ namespace Rito
         private Texture refreshTexture;
         private string texturePath;
 
+        private object _localRotationGUI; // 사용 이유 : 안쓰면 Euler.x 짐벌락 걸림
+        private System.Reflection.MethodInfo _trguiOnEnableMethod;
+        private System.Reflection.MethodInfo _trguiRotationFieldMethod;
+
         private void OnEnable()
         {
             transform = target as Transform;
 
             texturePath = TransformEditorHelper.folderPath + @"\Refresh.png";
             refreshTexture = AssetDatabase.LoadAssetAtPath(texturePath, typeof(Texture2D)) as Texture2D;
+
+            // 치트키 : 기존 TransformEditor로부터 RotationField 빌려쓰기
+            if (_localRotationGUI == null)
+            {
+                var bunch = TransformEditorHelper.GetTransformRotationGUI();
+                _localRotationGUI = bunch.rotationGUI;
+                _trguiOnEnableMethod = bunch.OnEnable;
+                _trguiRotationFieldMethod = bunch.RotationField;
+            }
+            _trguiOnEnableMethod.Invoke(_localRotationGUI, new object[] {
+                base.serializedObject.FindProperty("m_LocalRotation"), EditorGUIUtility.TrTextContent("Local Rotation")
+            });
         }
 
         public override void OnInspectorGUI()
@@ -33,62 +49,76 @@ namespace Rito
             GUI.contentColor = Color.white;
             if (GUILayout.Button("Reset"))
             {
-                transform.localPosition = default;
-                transform.localRotation = default;
+                transform.localPosition = Vector3.zero;
                 transform.localScale = Vector3.one;
+
+                TransformUtils.SetInspectorRotation(transform, Vector3.zero);
             }
             GUI.backgroundColor = oldBgColor;
             GUI.contentColor = oldTxtColor;
 
-            DrawBasicTransformInspector();
+            base.serializedObject.Update();
+
+            DrawDefaultTransformInspector();
 
             EditorGUILayout.Space();
             if (advancedFoldOut = EditorGUILayout.Foldout(advancedFoldOut, "Advanced"))
             {
                 DrawGlobalTransformValues();
             }
+
+            base.serializedObject.ApplyModifiedProperties();
         }
 
-        private void DrawBasicTransformInspector()
+        private void DrawDefaultTransformInspector()
         {
+            // ==================================== Local Position =========================================
             EditorGUILayout.BeginHorizontal();
 
             Undo.RecordObject(transform, "Transform Local Position Changed");
             transform.localPosition = EditorGUILayout.Vector3Field("Local Position", transform.localPosition);
+            transform.localPosition = Vector3E4Clamp(transform.localPosition);
 
             Color oldBgColor = GUI.backgroundColor;
             GUI.backgroundColor = Color.green;
             if (GUILayout.Button(refreshTexture, GUILayout.Width(20f), GUILayout.Height(18f)))
             {
-                transform.localPosition = default;
+                transform.localPosition = Vector3.zero;
             }
             GUI.backgroundColor = oldBgColor;
             EditorGUILayout.EndHorizontal();
 
-            //=============================================================================================
-
+            // ==================================== Local Rotation =========================================
             EditorGUILayout.BeginHorizontal();
 
             Undo.RecordObject(transform, "Transform Local Rotation Changed");
-            var localRot = EditorGUILayout.Vector3Field("Local Rotation", transform.localEulerAngles);
-            transform.localEulerAngles = localRot;
-            //transform.localEulerAngles = new Vector3(Mathf.Round(localRot.x * 100f) * 0.01f, Mathf.Round(localRot.y * 100f) * 0.01f, Mathf.Round(localRot.z * 100f) * 0.01f);
+
+            _trguiRotationFieldMethod.Invoke(_localRotationGUI, new object[] { });
+
+            // 0~360 값 벗어나면 제한
+            Vector3 exposedLocalEulerAngle = TransformUtils.GetInspectorRotation(transform);
+            if (exposedLocalEulerAngle.x < 0f || exposedLocalEulerAngle.y < 0f || exposedLocalEulerAngle.z < 0f ||
+                exposedLocalEulerAngle.x > 360f || exposedLocalEulerAngle.y > 360f || exposedLocalEulerAngle.z > 360f)
+            {
+                TransformUtils.SetInspectorRotation(transform, transform.localEulerAngles);
+            }
 
             oldBgColor = GUI.backgroundColor;
             GUI.backgroundColor = Color.green;
             if (GUILayout.Button(refreshTexture, GUILayout.Width(20f), GUILayout.Height(18f)))
             {
-                transform.localRotation = default;
+                TransformUtils.SetInspectorRotation(transform, Vector3.zero);
             }
             GUI.backgroundColor = oldBgColor;
 
             EditorGUILayout.EndHorizontal();
-            //=============================================================================================
 
+            // ==================================== Local Scale =========================================
             EditorGUILayout.BeginHorizontal();
 
             Undo.RecordObject(transform, "Transform Local Scale Changed");
             transform.localScale = EditorGUILayout.Vector3Field("Local Scale", transform.localScale);
+            transform.localScale = Vector3E4Clamp(transform.localScale);
 
             oldBgColor = GUI.backgroundColor;
             GUI.backgroundColor = Color.green;
@@ -108,16 +138,15 @@ namespace Rito
             GUI.contentColor = Color.white;
             if (GUILayout.Button("Reset"))
             {
-                transform.position = default;
-                transform.rotation = default;
+                transform.position = Vector3.zero;
+                transform.eulerAngles = Vector3.zero;
                 ChangeGlobalScale(Vector3.one);
             }
             GUI.backgroundColor = oldBgColor;
             GUI.contentColor = oldTxtColor;
 
 
-            //=============================================================================================
-
+            // ==================================== Global Position =========================================
             EditorGUILayout.BeginHorizontal();
 
             Undo.RecordObject(transform, "Transform Global Position Changed");
@@ -127,30 +156,29 @@ namespace Rito
             GUI.backgroundColor = Color.blue;
             if (GUILayout.Button(refreshTexture, GUILayout.Width(20f), GUILayout.Height(18f)))
             {
-                transform.position = default;
+                transform.position = Vector3.zero;
             }
             GUI.backgroundColor = oldBgColor;
             EditorGUILayout.EndHorizontal();
 
-            //=============================================================================================
-
+            // ==================================== Global Rotation =========================================
             EditorGUILayout.BeginHorizontal();
 
             Undo.RecordObject(transform, "Transform Global Rotation Changed");
             var globalRot = EditorGUILayout.Vector3Field("Global Rotation", transform.eulerAngles);
-            //transform.eulerAngles = new Vector3(Mathf.Round(globalRot.x * 100f) * 0.01f, Mathf.Round(globalRot.y * 100f) * 0.01f, Mathf.Round(globalRot.z * 100f) * 0.01f);
+            transform.eulerAngles = Vector3E4Clamp(globalRot);
 
             oldBgColor = GUI.backgroundColor;
             GUI.backgroundColor = Color.blue;
             if (GUILayout.Button(refreshTexture, GUILayout.Width(20f), GUILayout.Height(18f)))
             {
-                transform.rotation = default;
+                //transform.rotation = default; // 안됨. 아래처럼 해야 함
+                transform.eulerAngles = Vector3.zero;
             }
             GUI.backgroundColor = oldBgColor;
             EditorGUILayout.EndHorizontal();
 
-            //=============================================================================================
-
+            // ==================================== Global Scale =========================================
             EditorGUILayout.BeginHorizontal();
 
             Undo.RecordObject(transform, "Transform Global Scale Changed");
@@ -188,6 +216,14 @@ namespace Rito
             }
 
             transform.localScale = globalScale;
+        }
+
+        private Vector3 Vector3E4Clamp(Vector3 vector)
+        {
+            vector.x = Mathf.Round(vector.x * 10000f) * 0.0001f;
+            vector.y = Mathf.Round(vector.y * 10000f) * 0.0001f;
+            vector.z = Mathf.Round(vector.z * 10000f) * 0.0001f;
+            return vector;
         }
     }
 }
--- Rito/2. Toy/2021_0218_TransformEditor/TransformEditorHelper.cs
+++ Rito/2. Toy/2021_0218_TransformEditor/TransformEditorHelper.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using System.Reflection;
 
 // 날짜 : 2021-02-18 AM 4:41:01
 // 작성자 : Rito
@@ -16,7 +18,7 @@ namespace Rito
             GetFolderPath();
         }
 
-        static void GetFolderPath([System.Runtime.CompilerServices.CallerFilePath] string sourceFilePath = "")
+        private static void GetFolderPath([System.Runtime.CompilerServices.CallerFilePath] string sourceFilePath = "")
         {
             folderPath = System.IO.Path.GetDirectoryName(sourceFilePath);
             int rootIndex = folderPath.IndexOf(@"Assets\");
@@ -24,6 +26,24 @@ namespace Rito
             {
                 folderPath = folderPath.Substring(rootIndex, folderPath.Length - rootIndex);
             }
+        }
+
+        public static (object rotationGUI, MethodInfo OnEnable, MethodInfo RotationField) GetTransformRotationGUI()
+        {
+            object target;
+            MethodInfo onEnableMethod;
+            MethodInfo rotationFieldMethod;
+
+            Type targetType = Type.GetType("UnityEditor.TransformRotationGUI, UnityEditor");
+            target = Activator.CreateInstance(targetType);
+
+            onEnableMethod = targetType.GetMethod (
+                "OnEnable",
+                new Type[] { typeof(UnityEditor.SerializedProperty), typeof(GUIContent) }
+            );
+            rotationFieldMethod = targetType.GetMethod("RotationField", new Type[] { });
+
+            return (target, onEnableMethod, rotationFieldMethod);
         }
     }
 }
